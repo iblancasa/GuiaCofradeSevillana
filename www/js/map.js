2128346b8d8d98de316e9257f97e8f2077d7c338
Israel Blancas <iblancasa@gmail.com>
2015-03-19 13:46:41 +0100

    Añadida la primera Hermandad

--- www/js/map.js
+++ www/js/map.js
@@ -1,14 +1,32 @@
 var map;
 function initialize() {
 var mapOptions = {
-  zoom: 8,
-  center: new google.maps.LatLng(-34.397, 150.644)
+  zoom: 15,
+  center: new google.maps.LatLng(37.3890118,-5.9842985)
 };
 map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
 
 document.getElementById('map-canvas').style.zIndex = "1";
 document.getElementById('header').style.zIndex = "10";
+
+
+var image = 'img/hermandades/madruga/triana/escudo.png';
+  var sevilla = new google.maps.LatLng(37.384,-6.001351);
+  var giralda = new google.maps.Marker({
+    position: sevilla,
+    map: map,
+    icon: image
+  });
+
+  google.maps.event.addListener(giralda, 'click', function() {
+  $("#hermandad").fadeIn( "slow");
+});
+
+
+
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
