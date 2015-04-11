da5d13b47e1b73c1c3fa98aa90a0804e897013e5
Israel Blancas <iblancasa@gmail.com>
2015-04-11 13:31:21 +0200

    Reestructurando index

---
+++ www/js/helpers.js
@@ -0,0 +1,79 @@
+
+$(function() {
+	var stemplate =  $.ajax({
+	url: 'plantillahermandad.html'
+	});
+
+	stemplate.done(function (data) {
+    var tmpl = Handlebars.compile(data);
+
+		var xhr = $.ajax({
+		url: 'Beers.json'
+		});
+
+		xhr.done(function (data) {
+      console.log(data);
+			var ctx = data;
+			var html = tmpl(ctx);
+			$("#hermandad").html(html);
+      $( "#cerrar" ).click(function() {
+        $("#hermandad").fadeOut(500);
+      });
+		});
+	});
+
+});
+
+
+/**
+* Devuelve la ruta al escudo de la Hermandad
+* @param {String} Ruta a la carpeta
+* @returns {String} Ruta al escudo
+*/
+Handlebars.registerHelper('escudo', function(carpeta) {
+  return new Handlebars.SafeString(
+    carpeta + "escudo.png"
+  );
+});
+
+/**
+* Devuelve la ruta a la cabecera de la Hermandad
+* @param {String} Ruta a la carpeta
+* @returns {String} Ruta a la cabecera
+*/
+Handlebars.registerHelper('cabecera', function(carpeta) {
+  return new Handlebars.SafeString(
+    carpeta + "cabecera.jpg"
+  );
+});
+
+
+
+/**
+* Devuelve la ruta a la imagen del titular
+* @param {String} Ruta a la carpeta
+* @param {int} Índice del titular
+* @returns {String} Ruta a la imagen del titular según corresponda
+*/
+Handlebars.registerHelper('titular', function(carpeta,indice) {
+  var titular = null;
+
+  switch(indice) {
+    case 0:
+        titular = "primero.png";
+        break;
+    case 1:
+        titular = "segundo.png";
+        break;
+    case 2:
+        titular = "tercero.png";
+        break;
+    case 3:
+        titular = "cuarto.png";
+        break;
+    }
+
+  return new Handlebars.SafeString(
+    carpeta + titular
+  );
+});
